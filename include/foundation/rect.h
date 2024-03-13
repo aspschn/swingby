fd7666d32cbf9db00878dc602d83e84f073d9f27
Aspen Schneider <hardboiled65@gmail.com>
2024-03-14 08:06:10 +0900

    Check a rect contains the given point.

--- include/foundation/rect.h
+++ include/foundation/rect.h
@@ -1,6 +1,8 @@
 #ifndef _FOUNDATION_RECT_H
 #define _FOUNDATION_RECT_H
 
+#include <stdbool.h>
+
 #include <foundation/point.h>
 #include <foundation/size.h>
 
@@ -8,5 +10,7 @@ typedef struct ft_rect_t {
     ft_point_t pos;
     ft_size_t size;
 } ft_rect_t;
+
+bool ft_rect_contains_point(ft_rect_t *rect, const ft_point_t *point);
 
 #endif /* _FOUNDATION_RECT_H */
---
+++ src/rect.c
@@ -0,0 +1,31 @@
+#include <foundation/rect.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool ft_rect_contains_point(ft_rect_t *rect, const ft_point_t *point)
+{
+    // Check top-left.
+    if (point->x < rect->pos.x || point->y < rect->pos.y) {
+        return false;
+    }
+
+    // Check bottom-right.
+    if (point->x > rect->pos.x + rect->size.width &&
+        point->y > rect->pos.y + rect->size.height) {
+        return false;
+    }
+
+    // Check width and height.
+    if (point->x <= rect->pos.x + rect->size.width &&
+        point->y <= rect->pos.y + rect->size.height) {
+        return true;
+    }
+
+    return false;
+}
+
+#ifdef __cplusplus
+}
+#endif
