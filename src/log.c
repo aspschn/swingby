350ebeb22fde921c6f2f563381a72f654b0b110f
Aspen Schneider <hardboiled65@gmail.com>
2024-03-17 15:59:07 +0900

    Warn and error log functions implemented.

--- src/log.c
+++ src/log.c
@@ -16,9 +16,11 @@ void ft_log(ft_log_level level, const char *format, va_list args)
         fflush(stdout);
 #endif
     } else if (level == FT_LOG_LEVEL_WARN) {
-        //
+        fprintf(stderr, "\033[1;33m[WARN]\033[0m ");
+        vfprintf(stderr, format, args);
     } else if (level == FT_LOG_LEVEL_ERROR) {
-        //
+        fprintf(stderr, "\033[1;33m[ERROR]\033[0m ");
+        vfprintf(stderr, format, args);
     }
 }
 
@@ -28,6 +30,26 @@ void ft_log_debug(const char *format, ...)
     va_start(args, format);
 
     ft_log(FT_LOG_LEVEL_DEBUG, format, args);
+
+    va_end(args);
+}
+
+void ft_log_warn(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    ft_log(FT_LOG_LEVEL_WARN, format, args);
+
+    va_end(args);
+}
+
+void ft_log_error(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    ft_log(FT_LOG_LEVEL_ERROR, format, args);
 
     va_end(args);
 }
