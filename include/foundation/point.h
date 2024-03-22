1ab2565af20496483810d959d8c64624ceff052c
Aspen Schneider <hardboiled65@gmail.com>
2024-03-23 05:48:31 +0900

    Integer point and rect structs.

--- include/foundation/point.h
+++ include/foundation/point.h
@@ -1,9 +1,24 @@
 #ifndef _FOUNDATION_POINT_H
 #define _FOUNDATION_POINT_H
 
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct ft_point_t {
     float x;
     float y;
 } ft_point_t;
+
+typedef struct ft_point_i_t {
+    uint64_t x;
+    uint64_t y;
+} ft_point_i_t;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _FOUNDATION_POINT_H */
--- include/foundation/rect.h
+++ include/foundation/rect.h
@@ -6,11 +6,24 @@
 #include <foundation/point.h>
 #include <foundation/size.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct ft_rect_t {
     ft_point_t pos;
     ft_size_t size;
 } ft_rect_t;
 
+typedef struct ft_rect_i_t {
+    ft_point_i_t pos;
+    ft_size_i_t size;
+} ft_rect_i_t;
+
 bool ft_rect_contains_point(ft_rect_t *rect, const ft_point_t *point);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _FOUNDATION_RECT_H */
