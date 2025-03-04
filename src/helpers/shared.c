ff16affe8964178ab043f69bc053e138895bd2f6
Aspen Schneider <rendezvous71@outlook.com>
2025-03-05 06:18:19 +0900

    More shared helper functions.
    Move _find_most_child definition from wayland/application.c.
    This is also useful for other platforms.

--- src/helpers/shared.c
+++ src/helpers/shared.c
@@ -1,7 +1,11 @@
 #include "shared.h"
 
+#include <stddef.h>
+
 #include <swingby/list.h>
 #include <swingby/event.h>
+#include <swingby/view.h>
+#include <swingby/point.h>
 
 void _event_listener_filter_for_each(sb_list_t *listeners,
                                      enum sb_event_type type,
@@ -14,4 +18,24 @@ void _event_listener_filter_for_each(sb_list_t *listeners,
             tuple->listener(event);
         }
     }
+}
+
+sb_view_t* _find_most_child(sb_view_t *view,
+                            sb_point_t *position)
+{
+    sb_list_t *children = sb_view_children(view);
+
+    if (sb_list_length(children) == 0) {
+        return view;
+    }
+    sb_view_t *child = sb_view_child_at(view, position);
+
+    if (child == NULL) {
+        return view;
+    }
+
+    position->x = position->x - sb_view_geometry(child)->pos.x;
+    position->y = position->y - sb_view_geometry(child)->pos.y;
+
+    return _find_most_child(child, position);
 }
--- src/helpers/shared.h
+++ src/helpers/shared.h
@@ -4,6 +4,8 @@
 typedef struct sb_list_t sb_list_t;
 enum sb_event_type;
 typedef struct sb_event_t sb_event_t;
+typedef struct sb_view_t sb_view_t;
+typedef struct sb_point_t sb_point_t;
 
 #ifdef __cplusplus
 extern "C" {
@@ -12,6 +14,10 @@ extern "C" {
 void _event_listener_filter_for_each(sb_list_t *listeners,
                                      enum sb_event_type type,
                                      sb_event_t *event);
+
+/// \brief Find most child view of the root view.
+sb_view_t* _find_most_child(sb_view_t *view,
+                            sb_point_t *position);
 
 #ifdef __cplusplus
 }
